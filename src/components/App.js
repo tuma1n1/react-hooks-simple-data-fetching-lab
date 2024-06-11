95470f30340a1196eed672e229bc40603c479a8f
Tumaini <tumainikebaya.234@gmail.com>
2024-06-11 20:14:41 +0300

    Adds App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, {useState, useEffect} from 'react';
+
+function App() {
+    const [dog, setDog] = useState('');
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) =>  {
+            setDog(data.message);
+            setIsLoaded(true);
+        });
+    }, []);
+
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+    <div>
+        <img src={dog} alt='A Random Dog' />
+    </div>
+    );
+}
+
+
+export default App;
